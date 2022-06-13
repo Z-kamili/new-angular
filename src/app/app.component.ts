86511271896f3aee8b751e7c2b2f9e0bec394d36
Z-Kamili <zakariakamili97@gmail.com>
2022-06-13 16:14:00 +0100

    add feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-new';
   myname = "Zakaria";
-  myInfo:boolean = false;
-  myName:string = "Alice";
+  myInfo: boolean = false;
+  myName: string = "Alice";
+
+  customerArray = [1, 2, 3, 4, 5, 6, 7];
+
+  personObject = 
+  [
+    {
+        name: 'Ebony',
+        age: 25,
+        city: 'ohayo'
+    },
+    {
+        name: 'Ebony',
+        age: 25,
+        city: 'ohayo'
+    },
+    {
+        name: 'Ebony',
+        age: 25,
+        city: 'ohayo'
+    },
+  ]
 
 
-  greet(myInfo: boolean)
-  {
-   console.log("hiiii");
-   this.myInfo = !myInfo;
-  }
+greet(myInfo: boolean)
+{
+  console.log("hiiii");
+  this.myInfo = !myInfo;
+}
 
-  onsave()
-  {
-    console.log("hii");
-  }
+onsave()
+{
+  console.log("hii");
+}
 
 
 
