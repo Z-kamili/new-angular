3f0bf7c506bc5e5b45b7a2da0ad625fcb9d42647
Z-Kamili <zakariakamili97@gmail.com>
2022-06-13 17:41:17 +0100

    add pipies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  mydate = new Date();
+  myJson = {name:"zakaria",age:25,department:"Backend developer",address:{city:'safi',phoneNumber:12308493}}
+  myArrayNum = [22,23,12,90,100];
+
+  printNumber:number = 12;
+  fullname:string = "Muhammed";
   title = 'angular-new';
   myname = "Zakaria";
   myInfo: boolean = false;
   myName: string = "Alice";
+  someText:string = "You can change me";
 
   customerArray = [1, 2, 3, 4, 5, 6, 7];
 
@@ -33,16 +42,21 @@ export class AppComponent {
   ]
 
 
-greet(myInfo: boolean)
-{
-  console.log("hiiii");
-  this.myInfo = !myInfo;
-}
+   greet(myInfo: boolean)
+   {
+     console.log("hiiii");
+     this.myInfo = !myInfo;
+   }
 
-onsave()
-{
-  console.log("hii");
-}
+    sum()
+    {
+      return  this.printNumber + 34; 
+    }
+
+    onsave()
+    {
+      console.log("hii");
+    }
 
 
 
